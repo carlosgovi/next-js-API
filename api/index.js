967ccde6ecddbcaa3346dac233ce1a4a3cfe2766
carlosgovi <cgovi66@gmail.com>
2023-03-08 21:15:18 -0300

    add archives for api

--- api/index.js
+++ api/index.js
@@ -2,5 +2,10 @@ console.log("index api");
 export default function handler(req, res) {
   res
     .status(200)
-    .json({ body: req.body, query: req.query, cookies: req.cookies });
+    .json({
+      body: req.body,
+      query: req.query,
+      cookies: req.cookies,
+      index: true,
+    });
 }
--- api/test.js
+++ api/test.js
@@ -1,1 +1,9 @@
 console.log("test");
+export default function handler(req, res) {
+  res.status(200).json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+    test: true,
+  });
+}
